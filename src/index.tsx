ae31d7d7ae6dda53c21f3c03cf977a469021b7df
Mikhail Linker <work.michaellinker@gmail.com>
2021-10-25 13:48:26 +0300

    NS-34 Add basic files and structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import store from './store/store';
 
 import './index.css';
 
-ReactDOM.render(<React.StrictMode />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
---
+++ src/reducers/rootReducer.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+// Add reducers here
+export const rootReducer = combineReducers({});
+
+export type AppState = ReturnType<typeof rootReducer>;
