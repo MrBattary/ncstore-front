b14abc1d51f5d3527e06e3f1f17ef5d8c2fc1915
Mikhail Linker <work.michaellinker@gmail.com>
2021-10-27 17:37:08 +0300

    NC-38 Sign action types

--- src/actions/user/userActionTypes.ts
+++ src/actions/user/userActionTypes.ts
@@ -9,3 +9,8 @@ export const SIGN_UP_ERROR = 'SIGN_UP_ERROR';
 export const SIGN_IN_REQUEST = 'Request/SIGN_IN';
 export const SIGN_IN_RECEIVE = 'Receive/SIGN_IN';
 export const SIGN_IN_ERROR = 'SIGN_IN_ERROR';
+
+/* Sign out */
+
+export const SIGN_OUT_REQUEST = 'Request/SIGN_OUT';
+export const SIGN_OUT_RECEIVE = 'Receive/SIGN_OUT';
