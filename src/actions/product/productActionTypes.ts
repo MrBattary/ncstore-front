8a5a1f2ea36842e5793899c0a3eed612877102fe
Mikhail Linker <work.michaellinker@gmail.com>
2021-11-01 17:29:13 +0300

    NS-115 Added product action types

---
+++ src/actions/product/productActionTypes.ts
@@ -0,0 +1,5 @@
+/* Get products */
+
+export const GET_PRODUCTS_REQUEST = 'Request/GET_PRODUCTS';
+export const GET_PRODUCTS_RECEIVE = 'Receive/GET_PRODUCTS';
+export const GET_PRODUCTS_ERROR = 'GET_PRODUCTS_ERROR';
