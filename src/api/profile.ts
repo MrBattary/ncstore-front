b9e73c5fe1c74ac4b6cb14e995fd2251e91d5dc8
Afomin <284718@niuitmo.ru>
2021-11-18 00:34:06 +0300

    Changed Company and Person SubUrls according to API

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,15 +1,15 @@
 import { getHTTP } from '../fetcher/fetcher';
 import { combineUrls } from './utilities';
-import { companySubUrl, coreUrl, personSubUrl } from './urls';
+import { companyInfoSubUrl, coreUrl, personInfoSubUrl } from './urls';
 import { PersonProfile } from '../types/PersonProfile';
 import { CompanyProfile } from '../types/CompanyProfile';
 import headers from '../fetcher/headers';
 
 const getPersonProfile = (token: string) =>
-    getHTTP<PersonProfile>(combineUrls([coreUrl, personSubUrl]), headers.buildHeaderTokenAcceptJson(token));
+    getHTTP<PersonProfile>(combineUrls([coreUrl, personInfoSubUrl]), headers.buildHeaderTokenAcceptJson(token));
 
 const getCompanyProfile = (token: string) =>
-    getHTTP<CompanyProfile>(combineUrls([coreUrl, companySubUrl]), headers.buildHeaderTokenAcceptJson(token));
+    getHTTP<CompanyProfile>(combineUrls([coreUrl, companyInfoSubUrl]), headers.buildHeaderTokenAcceptJson(token));
 
 const profileApi = {
     getPersonProfile,
