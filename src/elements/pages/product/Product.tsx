908729e5b1ab16bde9b1715a4dc1a009fbacc463
Mikhail Linker <work.michaellinker@gmail.com>
2021-11-25 16:51:57 +0300

    NS-215 Finished product page

--- src/elements/pages/product/Product.tsx
+++ src/elements/pages/product/Product.tsx
@@ -5,6 +5,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppState } from '../../../reducers/rootReducer';
 import { getProduct } from '../../../actions/products/GetProduct';
 import { restoreDefaultProductsReducer } from '../../../actions/products/RestoreDefaultProductsReducer';
+import { Box, ButtonGroup, Container, Paper, Typography } from '@mui/material';
+
+import './style.css';
+import Stack from '@mui/material/Stack';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
 
 type productProps = {
     history: History;
@@ -30,19 +36,121 @@ const Product: React.FC<productProps> = ({ history }) => {
         // eslint-disable-next-line
     }, []);
 
+    const goToSupplierPage = (supplierId: string | null | undefined) => {
+        console.log(`Go to supplier with id: ${supplierId}`);
+    };
+
+    const goToCategory = (categoryName: string) => {
+        console.log(`Go to category with name: ${categoryName}`);
+    };
+
+    const handleBuy = () => {
+        console.log(`Buy product`);
+    };
+
+    const handleAddToCart = () => {
+        console.log(`Product add to cart`);
+    };
+
+    const renderProductPrice = () => {
+        if (!productInfo?.discountPrices.length) {
+            return (
+                <Stack direction='row'>
+                    <Typography variant='h4' color='text.secondary'>
+                        {productInfo?.normalPrices[0].price.toString()}
+                    </Typography>
+                </Stack>
+            );
+        } else {
+            return (
+                <Stack spacing={1} direction='row'>
+                    <Typography variant='h4' color='text.secondary'>
+                        {productInfo?.discountPrices[0].price.toString()}
+                    </Typography>
+                    <Typography
+                        sx={{ textDecoration: 'line-through', opacity: 0.5 }}
+                        variant='h4'
+                        color='text.secondary'
+                    >
+                        {productInfo?.normalPrices[0].price.toString()}
+                    </Typography>
+                </Stack>
+            );
+        }
+    };
+
+    const renderCategoriesButtons = () =>
+        productInfo?.categoriesNames.map((categoryName: string) => (
+            <Button onClick={() => goToCategory(categoryName)}>{categoryName}</Button>
+        ));
+
     const renderProductData = () => {
         return (
-            <>
-                <h1>{productInfo?.productName}</h1>
-            </>
+            <Container>
+                <Paper>
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            padding: 4,
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <div className='content__product-header'>
+                            <img
+                                className='product-header__image'
+                                src='/default-product-image.jpg'
+                                alt={`product ${productInfo?.productName}`}
+                            />
+                            <div className='product-header__fields'>
+                                <div className='fields__name-company'>
+                                    <Typography variant='h3' style={{ marginTop: 1 }}>
+                                        {productInfo?.productName}
+                                    </Typography>
+                                    <Link
+                                        variant='h4'
+                                        underline='none'
+                                        style={{ marginBottom: 2 }}
+                                        onClick={() => goToSupplierPage(productInfo?.supplierId)}
+                                    >
+                                        {productInfo?.supplierName}
+                                    </Link>
+                                </div>
+                                {renderProductPrice()}
+                                <div className='fields__buttons'>
+                                    <Button variant='contained' style={{ margin: 3 }} onClick={handleBuy}>
+                                        Buy now
+                                    </Button>
+                                    <Button variant='outlined' style={{ margin: 3 }} onClick={handleAddToCart}>
+                                        Add to cart
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='content__product-details'>
+                            <div className='product-details__description'>
+                                <Typography variant='h6' style={{ marginBottom: 1 }}>
+                                    Product description:
+                                </Typography>
+                                <Typography variant={'body2'} style={{ marginBottom: 1 }}>
+                                    {productInfo?.productDescription}
+                                </Typography>
+                            </div>
+                            <ButtonGroup variant='text'>{renderCategoriesButtons()}</ButtonGroup>
+                        </div>
+                    </Box>
+                </Paper>
+            </Container>
         );
     };
 
     const renderProductNotFound = () => {
         return (
-            <>
-                <h1>NOT FOUND</h1>
-            </>
+            <div className='content__product-not-found'>
+                <Typography className='product-not-found__label' variant='h4' display='inline-block'>
+                    Oops, we can't find this product...
+                </Typography>
+            </div>
         );
     };
 
