563eeff77286ce34bcc1f1bc97c842515d2e03f3
Afomin <284718@niuitmo.ru>
2021-11-28 00:09:40 +0300

    Added general profile

---
+++ src/elements/components/profiles/GeneralProfile.tsx
@@ -0,0 +1,170 @@
+import React from 'react';
+import {History} from 'history';
+
+import BusinessIcon from '@mui/icons-material/Business';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import {Avatar, Box, Container, List, ListItem, ListItemText, Paper, Stack, Typography} from '@mui/material';
+
+import {PersonProfile} from '../../../types/PersonProfile';
+import {CompanyProfile} from '../../../types/CompanyProfile';
+import {UserRole} from '../../../types/UserRole';
+
+type profileProps = {
+    history: History;
+    profile: PersonProfile | CompanyProfile;
+};
+
+const Profile: React.FC<profileProps> = ({history, profile}) => {
+    const renderTypographyInProfile = (data: any) => (
+        <Typography align="center" component='h1' variant='h5'>
+            {data}
+        </Typography>
+    );
+
+    const renderHeaderTypographyInProfile = (data: any) => (
+        <Typography align="center" variant='h4'>
+            {data}
+        </Typography>
+    );
+
+    const renderPersonNickname = () => {
+        if ((profile as PersonProfile).nickName) {
+            return (
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    <Avatar sx={{bgcolor: 'secondary.main', marginTop: 2, width: 140, height: 140}}>
+                        <AccountCircle sx={{width: 120, height: 120}}/>
+                    </Avatar>
+                    <Typography align="center" variant='h3'>
+                        {(profile as PersonProfile).nickName}
+                    </Typography>
+                </Box>
+            );
+        }
+    };
+
+    const renderCompanyName = () => {
+        if ((profile as CompanyProfile).companyName) {
+            return (
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    <Avatar sx={{bgcolor: 'secondary.main', marginTop: 2, marginBottom: 4, width: 140, height: 140}}>
+                        <BusinessIcon sx={{width: 120, height: 120}}/>
+                    </Avatar>
+                    <Typography align="center" variant='h3'>
+                        {(profile as CompanyProfile).companyName}
+                    </Typography>
+                </Box>
+            );
+        }
+    };
+
+    const renderCompanyDescription = () => {
+        if ((profile as CompanyProfile).description) {
+            return (
+                <>
+                    {renderHeaderTypographyInProfile("About company")}
+                    {renderTypographyInProfile((profile as CompanyProfile).description)}
+                </>
+            );
+        }
+    };
+
+    const renderPersonNames = () => {
+        if ((profile as PersonProfile).firstName && (profile as PersonProfile).lastName) {
+            return renderTypographyInProfile(
+                <>
+                    {renderHeaderTypographyInProfile("First name")}
+                    {renderTypographyInProfile((profile as PersonProfile).firstName)}
+                    {renderHeaderTypographyInProfile("Last name")}
+                    {renderTypographyInProfile((profile as PersonProfile).lastName)}
+                </>
+            );
+        }
+        if ((profile as PersonProfile).firstName) {
+            return renderTypographyInProfile(
+                <>
+                    {renderHeaderTypographyInProfile("First name")}
+                    {renderTypographyInProfile((profile as PersonProfile).firstName)}
+                </>
+            );
+        }
+        if ((profile as PersonProfile).lastName) {
+            return renderTypographyInProfile(
+                <>
+                    {renderHeaderTypographyInProfile("Last name")}
+                    {renderTypographyInProfile((profile as PersonProfile).lastName)}
+                </>
+            );
+        }
+    };
+
+    const renderPersonBirthday = () => {
+        if ((profile as PersonProfile).birthday) {
+            return renderTypographyInProfile(
+                <>
+                    {renderHeaderTypographyInProfile("Birthday")}
+                    {renderTypographyInProfile((profile as PersonProfile).birthday)}
+                </>
+            );
+        }
+    };
+
+    const renderCompanyFoundationDate = () => {
+        if ((profile as CompanyProfile).foundationDate) {
+            return renderTypographyInProfile(
+                <>
+                    {renderHeaderTypographyInProfile("Foundation date")}
+                    {renderTypographyInProfile((profile as CompanyProfile).foundationDate)}
+                </>
+            );
+        }
+    };
+
+    const renderRolesList = () => {
+        if (profile) {
+            return profile.roles
+                ? profile.roles.map((role: UserRole) => (
+                    <ListItem>
+                        <ListItemText
+                            primary={(role: UserRole) => role.charAt(0).toUpperCase() + role.slice(1).toLowerCase()}
+                        />
+                    </ListItem>
+                ))
+                : null;
+        }
+    };
+
+    return (
+        <Container>
+            <Paper elevation={10}>
+                <Box sx={{
+                    marginTop: 8,
+                    paddingTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}>
+                    <Stack spacing={5}>
+                        <Typography align="center" variant="h2">Profile Details</Typography>
+                        {renderPersonNickname()}
+                        {renderCompanyName()}
+                        {renderCompanyDescription()}
+                        {renderPersonNames()}
+                        {renderPersonBirthday()}
+                        {renderCompanyFoundationDate()}
+                        <List>{renderRolesList()}</List>
+                    </Stack>
+                </Box>
+            </Paper>
+        </Container>
+    );
+};
+
+export default Profile;
--- src/elements/pages/profile/Profile.tsx
+++ src/elements/pages/profile/Profile.tsx
@@ -2,8 +2,7 @@ import React, { useEffect } from 'react';
 import { History } from 'history';
 import { useDispatch, useSelector } from 'react-redux';
 
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { Avatar, Container, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+import {Container} from '@mui/material';
 import { useSnackbar } from 'notistack';
 
 import { AppState } from '../../../reducers/rootReducer';
@@ -11,9 +10,7 @@ import { restoreDefaultUserReducer } from '../../../actions/users/RestoreDefault
 import { getPersonProfile } from '../../../actions/users/GetPersonProfile';
 import { getCompanyProfile } from '../../../actions/users/GetCompanyProfile';
 import { UserType } from '../../../types/UserType';
-import { PersonProfile } from '../../../types/PersonProfile';
-import { CompanyProfile } from '../../../types/CompanyProfile';
-import { UserRole } from '../../../types/UserRole';
+import GeneralProfile from "../../components/profiles/GeneralProfile";
 
 type profileProps = {
     history: History;
@@ -54,85 +51,11 @@ const Profile: React.FC<profileProps> = ({ history }) => {
         }
     }, [token, history]);
 
-    const renderTypographyInProfile = (data: any) => (
-        <Typography component='h1' variant='h4'>
-            {data}
-        </Typography>
-    );
 
-    const renderPersonNickname = () => {
-        if ((profile as PersonProfile).nickName) {
-            return renderTypographyInProfile((profile as PersonProfile).nickName);
-        }
-    };
-
-    const renderCompanyName = () => {
-        if ((profile as CompanyProfile).companyName) {
-            return renderTypographyInProfile((profile as CompanyProfile).companyName);
-        }
-    };
-
-    const renderCompanyDescription = () => {
-        if ((profile as CompanyProfile).description) {
-            return renderTypographyInProfile((profile as CompanyProfile).description);
-        }
-    };
-
-    const renderPersonNames = () => {
-        if ((profile as PersonProfile).firstName && (profile as PersonProfile).lastName) {
-            return renderTypographyInProfile(
-                (profile as PersonProfile).firstName.concat((profile as PersonProfile).lastName)
-            );
-        }
-        if ((profile as PersonProfile).firstName) {
-            return renderTypographyInProfile((profile as PersonProfile).firstName);
-        }
-        if ((profile as PersonProfile).lastName) {
-            return renderTypographyInProfile((profile as PersonProfile).lastName);
-        }
-    };
-
-    const renderPersonBirthday = () => {
-        if ((profile as PersonProfile).birthday) {
-            return renderTypographyInProfile((profile as PersonProfile).birthday);
-        }
-    };
-
-    const renderCompanyFoundationDate = () => {
-        if ((profile as CompanyProfile).foundationDate) {
-            return renderTypographyInProfile((profile as CompanyProfile).foundationDate);
-        }
-    };
-
-    const renderRolesList = () => {
-        if (profile) {
-            return profile.roles
-                ? profile.roles.map((role: UserRole) => (
-                      <ListItem>
-                          <ListItemText
-                              primary={(role: UserRole) => role.charAt(0).toUpperCase() + role.slice(1).toLowerCase()}
-                          />
-                      </ListItem>
-                  ))
-                : null;
-        }
-    };
 
     return loading || !profile ? null : (
         <Container>
-            <Paper>
-                <Avatar sx={{ bgcolor: 'secondary.main', marginTop: 2 }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                {renderPersonNickname()}
-                {renderCompanyName()}
-                {renderTypographyInProfile(profile.balance ? profile.balance : null)}
-                {renderCompanyDescription()}
-                {renderPersonNames()}
-                {renderPersonBirthday()}
-                {renderCompanyFoundationDate()}
-                <List>{renderRolesList()}</List>
-            </Paper>
+            <GeneralProfile history={history} profile={profile}/>
         </Container>
     );
 };
