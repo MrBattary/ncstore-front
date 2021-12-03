93715c7890709102ddca90caccd04ff53098b638
Mikhail Linker <work.michaellinker@gmail.com>
2021-12-03 12:04:17 +0300

    NS-234 Added action types

---
+++ src/actions/cart/cartActionTypes.ts
@@ -0,0 +1,23 @@
+/* Get cart */
+
+export const GET_CART_REQUEST = 'Request/GET_CART';
+export const GET_CART_RECEIVE = 'Receive/GET_CART';
+export const GET_CART_ERROR = 'GET_CART_ERROR';
+
+/* Add product to cart */
+
+export const ADD_PRODUCT_TO_CART_REQUEST = 'Request/ADD_PRODUCT_TO_CART';
+export const ADD_PRODUCT_TO_CART_RECEIVE = 'Receive/ADD_PRODUCT_TO_CART';
+export const ADD_PRODUCT_TO_CART_ERROR = 'ADD_PRODUCT_TO_CART_ERROR';
+
+/* Delete product from cart */
+
+export const DELETE_PRODUCT_FROM_CART_REQUEST = 'Request/DELETE_PRODUCT_FROM_CART';
+export const DELETE_PRODUCT_FROM_CART_RECEIVE = 'Receive/DELETE_PRODUCT_FROM_CART';
+export const DELETE_PRODUCT_FROM_CART_ERROR = 'DELETE_PRODUCT_FROM_CART_ERROR';
+
+/* Checkout cart */
+
+export const CHECKOUT_CART_REQUEST = 'Request/CHECKOUT_CART';
+export const CHECKOUT_CART_RECEIVE = 'Receive/CHECKOUT_CART';
+export const CHECKOUT_CART_ERROR = 'CHECKOUT_CART_ERROR';
